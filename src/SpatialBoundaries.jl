52411c998d4d298b21e22525ece4b5e4c003c31b
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-04 19:21:33 -0400

    drop dependencies that aren't required

--- src/SpatialBoundaries.jl
+++ src/SpatialBoundaries.jl
@@ -6,12 +6,7 @@ using CSV: CSV
 using DataFrames
 using Delaunay
 using LinearAlgebra
-using NeutralLandscapes
-using Plots
-using SimpleSDMLayers
-using SpatialEcology
 using Statistics
-using StatsPlots
 
 # Load and export types and function
 include(joinpath("lib", "rateofchange.jl")) # internal functions
