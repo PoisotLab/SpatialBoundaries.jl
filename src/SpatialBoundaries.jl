ddee1dabc372f7ccf575f000ac9ac48c7615ee22
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-04 19:44:44 -0400

    fix boundaries

--- src/SpatialBoundaries.jl
+++ src/SpatialBoundaries.jl
@@ -9,7 +9,9 @@ using StatsBase
 include(joinpath("lib", "rateofchange.jl"))
 
 include(joinpath("lib", "wombling.jl"))
+export wombling
 
 include(joinpath("lib", "boundaries.jl"))
+export boundaries
 
 end
--- src/lib/boundaries.jl
+++ src/lib/boundaries.jl
@@ -1,17 +1,12 @@
 """
     boundaries(M::Matrix{Union{Nothing, Float32}}; threshold::Float32=0.1)
 
-Extracts candidate boundaries using calculated rates of change (M) on specified 
-threshold. Default threshold is 10%.
+Extracts candidate boundaries using calculated rates of change (M) on specified
+threshold. Default threshold is 10%, meaning that the top 10% of pixels are
+selected as part of the boundaries.
 """
-function boundaries(M::Matrix{Union{Nothing,Float32}}; threshold=0.1)
-
-    rank = floor(Int, size(M, 2) * size(M, 1) * threshold)
-    M_n = denserank(replace(M, nothing => missing), # need to use type::missing
-                    rev=true) # ranks largest to smallest
-
-    replace!(x -> isless(x, rank) ? 1 : missing, M_n) # assigns all in above threshold to 1
-
-    # Rate of change and direction
-    return replace(M_n, missing => nothing) # back to type::nothing to play with SDMSimple
+function boundaries(M::Matrix{T}; threshold::T=0.1) where {T <: Number}
+    limit = floor(Int, size(M, 2) * size(M, 1) * threshold)
+    M_n = denserank(M, rev=true) # ranks largest to smallest
+    return M_n .< limit
 end
