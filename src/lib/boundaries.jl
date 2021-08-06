92ed902f35b6802f184f7721ab5fcd5b07059fc7
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-06 15:44:10 +0200

    :elephant: there is some fine-tuning that needs to happen. also discussion RE wombling -> boundaries pipeline

--- src/lib/boundaries.jl
+++ src/lib/boundaries.jl
@@ -10,3 +10,20 @@ function boundaries(M::Matrix{T}; threshold::T=0.1) where {T <: Number}
     M_n = denserank(M, rev=true) # ranks largest to smallest
     return M_n .< limit
 end
+
+"""
+    boundaries(M::Vector{Float64}, x::Vector{Float64}, y::Vector{Float64}; threshold::T=0.1) where {T <: Number}
+
+Extracts candidate boundaries using calculated rates of change (M) on specified
+threshold. Default threshold is 10%, meaning that the top 10% of pixels are
+selected as part of the boundaries.
+"""
+function boundaries(M::Vector{Float64}, x::Vector{Float64}, y::Vector{Float64}; threshold::T=0.1) where {T <: Number}
+    
+    limit = floor(Int, length(df.M) * threshold)
+    
+    v = hcat(denserank(M, rev = true),M, x, y); 
+    sort!(v, dims = 1, by = x -> x[1])
+    v[(v[:,1] .< limit),:]
+    return (B = v[:,2], x = v[:,3], y = v[:,4])
+end
