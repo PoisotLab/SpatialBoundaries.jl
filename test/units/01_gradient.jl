ae11aec3ba8b173f76d85884f5e8ed2cae104e9a
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-11 09:34:00 -0400

    🟩 tests for the dimension of LatticeWomble

--- test/units/01_gradient.jl
+++ test/units/01_gradient.jl
@@ -2,7 +2,16 @@ module SBTestGradient
 
     using SpatialBoundaries
     using NeutralLandscapes
+    using Test
 
-    landscape = rand(PlanarGradient(), 50, 50)
+    landscape = rand(PlanarGradient(), 40, 50)
+    x = collect(LinRange(0.2, 1.8, size(landscape, 1)))
+    y = collect(LinRange(0.2, 0.8, size(landscape, 2)))
+
+    W = wombling(x, y, landscape)
+    @test isa(W, LatticeWomble)
+    @test size(W.m) == size(landscape) .- 1
+    @test size(W.x) == length(x) - 1
+    @test size(W.y) == length(y) - 1
 
 end
