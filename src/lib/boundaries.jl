8a4eb30f8afc76a09642b21131d873a2899dc8da
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-11 09:49:20 -0400

    🧹  formatting

--- src/lib/boundaries.jl
+++ src/lib/boundaries.jl
@@ -5,15 +5,14 @@ Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
 selected as part of the boundaries.
 """
-function boundaries(W::TriangulationWomble{T}; threshold::T = 0.1) where {T<:Number}
-
+function boundaries(W::TriangulationWomble{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, length(W.m) * threshold)
 
-    changerank = findall(denserank(W, rev = true) .<= limit)
-    
+    changerank = findall(denserank(W; rev=true) .<= limit)
+
     candidate_boundaries = hcat(W.x[changerank], W.y[changerank], W.m[changerank])
-    sort!(candidate_boundaries, dims=1, by=last, rev=true)
-    
+    sort!(candidate_boundaries; dims=1, by=last, rev=true)
+
     return candidate_boundaries
 end
 
@@ -24,8 +23,8 @@ Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
 selected as part of the boundaries.
 """
-function boundaries(M::Matrix{T}; threshold::T = 0.1) where {T<:Number}
+function boundaries(M::Matrix{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, size(M, 2) * size(M, 1) * threshold)
-    M_n = denserank(M, rev = true) # ranks largest to smallest
+    M_n = denserank(M; rev=true) # ranks largest to smallest
     return M_n .< limit
 end
--- test/units/01_gradient.jl
+++ test/units/01_gradient.jl
@@ -1,17 +1,17 @@
 module SBTestGradient
 
-    using SpatialBoundaries
-    using NeutralLandscapes
-    using Test
+using SpatialBoundaries
+using NeutralLandscapes
+using Test
 
-    landscape = rand(PlanarGradient(), 40, 50)
-    x = collect(LinRange(0.2, 1.8, size(landscape, 1)))
-    y = collect(LinRange(0.2, 0.8, size(landscape, 2)))
+landscape = rand(PlanarGradient(), 40, 50)
+x = collect(LinRange(0.2, 1.8, size(landscape, 1)))
+y = collect(LinRange(0.2, 0.8, size(landscape, 2)))
 
-    W = wombling(x, y, landscape)
-    @test isa(W, LatticeWomble)
-    @test size(W.m) == size(landscape) .- 1
-    @test length(W.x) == length(x) - 1
-    @test length(W.y) == length(y) - 1
+W = wombling(x, y, landscape)
+@test isa(W, LatticeWomble)
+@test size(W.m) == size(landscape) .- 1
+@test length(W.x) == length(x) - 1
+@test length(W.y) == length(y) - 1
 
 end
