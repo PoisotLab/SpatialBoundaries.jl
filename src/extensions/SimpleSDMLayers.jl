2b20b9dcbc8029ea38d7e4f9b7aa7c869879e09f
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-13 14:06:56 -0400

    💬 document the SDM wombling

--- src/extensions/SimpleSDMLayers.jl
+++ src/extensions/SimpleSDMLayers.jl
@@ -1,5 +1,10 @@
 using SimpleSDMLayers
 
+"""
+    wombling(layer::T; convert_to::Type=Float64) where {T <: SimpleSDMLayer}
+
+Performs a lattice wombling on a `SimpleSDMLayer`.
+"""
 function wombling(layer::T; convert_to::Type=Float64) where {T <: SimpleSDMLayer}
     try
         global nan = convert(convert_to, NaN)
