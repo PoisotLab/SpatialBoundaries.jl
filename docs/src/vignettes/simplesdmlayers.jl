fd85d1626584ce79963d3fce645c346d62d12bb9
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-13 14:35:51 -0400

    🧑‍🏫 update figures in the SDM example

--- docs/src/vignettes/simplesdmlayers.jl
+++ docs/src/vignettes/simplesdmlayers.jl
@@ -9,6 +9,10 @@ using SimpleSDMLayers
 using SpatialBoundaries
 using StatsPlots
 
+# Note that the warning about dependencies is a side-effect of loading some
+# functionalities for `SimpleSDMLayers` as part of `SpatialBoundaries`, and can
+# safely be ignored.
+
 # In this example, we will look at precipitation data over the province
 # colonially refered to as Québec, and measure the rate of change, as well as
 # the direction of it. These data are extracted from the WorldClim database.
@@ -16,6 +20,11 @@ using StatsPlots
 precipitation = SimpleSDMPredictor(
     WorldClim, BioClim, 12; left=-80.0, right=-56.0, bottom=44.0, top=62.0
 )
+
+# We can have a look at this layer, after setting a few defaults:
+
+default(; dpi=500, size=(600, 600), aspectratio=1, c=:davos, frame=:box)
+plot(precipitation)
 
 # There is an overload of the `wombling` method for SDM layers, so we can call
 # it directly -- this method might result in a bit more memory usage than
@@ -31,10 +40,14 @@ stephist(
     deg2rad.(sort(vec(W.θ)));
     proj=:polar,
     lab="",
-    c=:black,
-    fill=(0, 0.3, :black),
+    c=:teal,
+    fill=(0, 0.2, :teal),
     nbins=100,
 )
+
+# The values for the direction of change are concentrated around 180° - note
+# that the direction of change is reported as a wind direction, meaning that
+# values tend to increase *from* the south *towards* the north.
 
 # We can also map the rate of change. This is far easier to do with a proper SDM
 # layer, so we will convert the wombling output:
@@ -46,4 +59,9 @@ Lr, Ld = SimpleSDMPredictor(W)
 # `SimpleSDMLayers`, but it makes the code a little easier to write, and so is
 # considered an acceptable trade-off.
 
-plot(Lr; c=:lapaz)
+plot(Lr)
+
+# We can finally have a look at the relationship between rate of change and
+# direction of change:
+
+scatter(deg2rad.(vec(W.θ)), vec(W.m); proj=:polar, zcolor=W.m, legend=false)
