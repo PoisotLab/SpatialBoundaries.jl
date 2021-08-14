603c7e05c2c11daf907f818e1e8aa42f353c870e
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-14 09:37:52 -0400

    💬 clarify docstrings for the updated boundaries function

--- src/lib/boundaries.jl
+++ src/lib/boundaries.jl
@@ -3,12 +3,12 @@
 
 Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
-selected as part of the boundaries.
+selected as part of the boundaries. This function returns a list of indices
+identifying which simplices are part of the boundaries.
 """
 function boundaries(W::TriangulationWomble{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, length(W.m) * threshold)
-    changerank = findall(denserank(W; rev=true) .<= limit)
-    return changerank
+    return findall(denserank(W; rev=true) .<= limit)
 end
 
 """
@@ -16,10 +16,11 @@ end
 
 Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
-selected as part of the boundaries.
+selected as part of the boundaries. This function returns an array of Cartesian
+indices corresponding to the rate of change matrix, indicating which positions
+are part of the boundaries.
 """
 function boundaries(W::LatticeWomble{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, size(W.m, 2) * size(W.m, 1) * threshold)
-    changerank = findall(denserank(W; rev=true) .<= limit)
-    return changerank
+    return findall(denserank(W; rev=true) .<= limit)
 end
