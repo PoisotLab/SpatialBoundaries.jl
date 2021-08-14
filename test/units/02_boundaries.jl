daabe830aa511edf3e237721436029f02f37bb39
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-14 09:45:41 -0400

    🟩 add test when there is a single value in the grid

--- test/units/02_boundaries.jl
+++ test/units/02_boundaries.jl
@@ -5,12 +5,14 @@ using Test
 
 A = fill(1.0, 10, 12)
 W = wombling(A)
-
 @test all(W.m .== zero(eltype(W.m)))
 @test all(W.θ .== zero(eltype(W.θ)))
-
 B = boundaries(W)
-
 @test length(B) == prod(size(A).-1)
+
+A[4, 8] = 2.0
+W = wombling(A)
+B = boundaries(W; threshold=0.05)
+@test length(B) == 4
 
 end
