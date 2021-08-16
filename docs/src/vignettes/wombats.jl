04bb47732c6b6837dd1b286922aff4a4269d1175
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-16 16:47:46 +0200

    :clapper: take 2 on that initial commit....

---
+++ docs/src/vignettes/wombats.jl
@@ -0,0 +1,11 @@
+## Wombling with wombats
+
+using GBIF
+using SpatialBoundaries
+
+# we can extract wombat occurence records using `GBIF.jl`
+
+occurrences(
+            taxon("Vombatus ursinus", rank = :SPECIES),
+            "limit" => 300
+            )
