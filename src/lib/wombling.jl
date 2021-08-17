17a9c0678917a1bd712caa1da4bf69ecb8608ac0
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-17 12:59:06 +0200

    :pencil2: spellings in docstring

--- src/lib/wombling.jl
+++ src/lib/wombling.jl
@@ -41,7 +41,7 @@ are regularly arranged in space. Note that the matrix is presented in a way that
 is flipped, *i.e.* the `x` coordinates corresponds to the rows, and the `y`
 coordinates correspond to the columns. If you want to think of `x` and `y` as
 geographic coordinates, `y` are the longitudes, and `x` are the latitudes. Using
-the bidings for `SimpleSDMLayers`, this conversion will be performed
+the bindings for `SimpleSDMLayers`, this conversion will be performed
 automatically.
 """
 function wombling(x::Vector{T}, y::Vector{T}, z::Matrix{T}) where {T<:Number}
