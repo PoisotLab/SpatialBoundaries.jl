a92bda839b454612817b7cf3807c75e84d2afd8e
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-23 09:44:46 -0400

    :books: a more expanded intro focus on interpretation

--- docs/src/vignettes/introduction.jl
+++ docs/src/vignettes/introduction.jl
@@ -1,5 +1,14 @@
 # # Introduction
 
+# ## Understanding Outputs
+
+# Broadly the Wombling algorthim traverses a given 'landscape' (for example species 
+# richness) and describes the the landscape in terms of the _Rate of Change (𝑀)_ 
+# (think slope/gradient) and _Direction of Change (θ)_ (direction of slope). 
+# **TODO FIGURE with a hypothetical landscpe but also schematic of the process**.
+
+# Something brief on the interpolation process?
+
 # In this example, we will see how the `SpatialBoundaries.jl` package works, by
 # taking a random mid-point displacement landscape, and measuring its rate and
 # direction of change.
@@ -39,8 +48,24 @@ W = wombling(landscape);
 
 heatmap(W.m, c=:bilbao, clim=(0, maximum(W.m)))
 
+# The rate of change informs us on the potential for there to be a boundary
+# (zone of change) within a specific cell. Cells with a high rate of change are
+# indicative of large differences (changes) in the landscape topology and are
+# suggestive of a boundary as we shift from one 'state' to another e.g.
+# transitioning from a protected to an urban area.
+
 # The *direction* of change is also given, and is expressed a *wind* direction;
 # for instance, an angle of 180° means that the value is smaller in the South,
 # and larger in the North:
 
 heatmap(W.θ, c=:brocO, clim=(0., 360.))
+
+# The direction of change is _not_ the direction the boundary would be if you
+# were to draw it on the landscape but rather the direction the rate of change
+# is 'moving in'. This means it is possible to think of and use the direction of
+# change independently of caluclating boundaries _per se_ and can be used to
+# inform how the landscape is behaving/changing in a more 'continuous' way as
+# opposed to discrete zones/boundaries. For example if changes in species
+# richness are more gradual (rate of change is near constant) but the the
+# direction of change is consistently South-North (i.e. 180°) we can still infer
+# that species richness is 'uniformly' increasing in a South-North direction.
