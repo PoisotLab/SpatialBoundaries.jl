f9258c5afef9d2b810179f0ef360ef8ccea2d710
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-08-25 10:29:16 -0400

    💬 mention that NaN don't count

--- src/lib/boundaries.jl
+++ src/lib/boundaries.jl
@@ -4,7 +4,8 @@
 Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
 selected as part of the boundaries. This function returns a list of indices
-identifying which simplices are part of the boundaries.
+identifying which simplices are part of the boundaries. The NaN values in the
+rates of change are not going to be a part of the boundaries.
 """
 function boundaries(W::TriangulationWomble{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, length(W.m) * threshold)
@@ -19,7 +20,8 @@ Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
 selected as part of the boundaries. This function returns an array of Cartesian
 indices corresponding to the rate of change matrix, indicating which positions
-are part of the boundaries.
+are part of the boundaries. The NaN values in the rates of change are not going
+to be a part of the boundaries.
 """
 function boundaries(W::LatticeWomble{T}; threshold::T=0.1) where {T<:Number}
     limit = floor(Int, size(W.m, 2) * size(W.m, 1) * threshold)
