1b3fa5dd07970cae6a766297d6736b7519d8ebe9
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-15 12:31:51 -0400

    🐛 fix bugs in wombat vignette

--- docs/src/vignettes/wombats.jl
+++ docs/src/vignettes/wombats.jl
@@ -4,36 +4,46 @@ using GBIF
 using SpatialBoundaries
 using SimpleSDMLayers
 using Statistics
+using StatsPlots
 
 # We can extract wombat occurence records using `GBIF.jl`
 
-observations = occurrences(taxon("Vombatus ursinus"; rank=:SPECIES), "country" => "AU")
+observations = occurrences(
+    taxon("Vombatus ursinus"; rank=:SPECIES),
+    "hasCoordinate" => true,
+    "decimalLatitude" => "-45,-20",
+    "decimalLongitude" => "130,160",
+    "limit" => 300,
+)
 
-while length(observations) <= min(2_500, size(observations))
-    occurences!(observations)
+while length(observations) <= min(10_000, size(observations))
+    occurrences!(observations)
 end
 
 # Let's grab some climate predictors
 
-aus_boundingbox = (left=110.0, right=160.0, bottom=-46.0, top=-8.0)
-layers = SimpleSDMPredictors(WorldClim, BioClim, 1:19; aus_boundingbox...)
+aus_boundingbox = (left=130.0, right=160.0, bottom=-45.0, top=-20.0)
+layers = SimpleSDMPredictor(WorldClim, BioClim, 1:19; aus_boundingbox...)
 
 # https://docs.ecojulia.org/SimpleSDMLayers.jl/stable/sdm/gbif/
 
-observation_density = mask(temperature, observations, Float32)
-observation_kernels = slidingwindow(observation_density, mean, 100.0)
+observation_density = mask(layers[1], observations, Float32)
+plot(observation_density)
 
 # ...
 
+observation_kernels = slidingwindow(observation_density, mean, 150.0)
 plot(observation_kernels)
 
 # ...
 
-Wr, Wd = SimpleSDMPredictor(wombling(observations_kernels))
+# ...
+
+Wr, Wd = SimpleSDMPredictor(wombling(observation_kernels))
 
 # ...
 
-heatmap(Wr; c=:nuuk)
+plot(Wr; c=:nuuk)
 
 # ...
 
