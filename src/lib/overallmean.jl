ae722841718855b3211cb805b9c2e56267c6a2cb
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-04-04 13:11:41 -0400

    🐛 return the direction in degrees

--- src/lib/overallmean.jl
+++ src/lib/overallmean.jl
@@ -4,13 +4,22 @@
 Overall Mean Lattice-Wombling Value, as in Fortin 1994
 """
 function omlwv(w::Vector{T}) where {T<:LatticeWomble}
-    # TODO assert that the coordinates are the same!
-    m = [layer.m for layer in w]
-    α = [deg2rad.(layer.θ) for layer in w]
-    average_change = reduce(.+, m) ./ length(w)
-    sin_comp = reduce(.+, sin.(α)) ./ length(w)
-    cos_comp = reduce(.+, cos.(α)) ./ length(w)
-    average_angle = atan.(sin_comp, cos_comp)
-    average_direction = rad2deg.(average_angle) .+ 180.0
-    return LatticeWomble(average_change, average_angle, w[1].x, w[1].y)
+    # Check that all wombles have the same dimensions and coordinates
+    @assert all([w1.x == w2.x for w1 in w, w2 in w])
+    @assert all([w1.y == w2.y for w1 in w, w2 in w])
+    @assert all([size(w1.m) == size(w2.m) for w1 in w, w2 in w])
+    # Prepare the matrices
+    m = fill(NaN, size(w[1].m))
+    α = fill(NaN, size(w[1].m))
+    # Fill the matrices
+    for _idx in eachindex(w[1].m)
+        ch = filter(!isnan, [womble.m[_idx] for womble in w])
+        di = filter(!isnan, [deg2rad(womble.θ[_idx]) for womble in w])
+        if !isempty(ch)
+            m[_idx] = mean(ch)
+            α[_idx] = atan(mean(sin.(di)), mean(cos.(di)))
+        end
+    end
+    average_direction = rad2deg.(α) .+ 180.0
+    return LatticeWomble(m, average_direction, w[1].x, w[1].y)
 end
