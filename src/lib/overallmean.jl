91cc89529ecc6e58869b85101268f7f90717fdf1
Tanya Strydom <tanya.strydom@icloud.com>
2022-04-15 15:47:20 -0400

    :bug: remove type MeanWomble

--- src/lib/overallmean.jl
+++ src/lib/overallmean.jl
@@ -3,7 +3,7 @@
 
 Overall Mean Lattice-Wombling Value, as in Fortin 1994
 """
-function omlwv(w::Vector{T}) where {T<:Womble}
+function omlwv(w::Vector{T}) where {T<:LatticeWomble}
     # Check that all wombles have the same dimensions and coordinates
     @assert all([w1.x == w2.x for w1 in w, w2 in w])
     @assert all([w1.y == w2.y for w1 in w, w2 in w])
@@ -21,5 +21,31 @@ function omlwv(w::Vector{T}) where {T<:Womble}
         end
     end
     average_direction = rad2deg.(α) .+ 180.0
-    return MeanWomble(m, average_direction, w[1].x, w[1].y)
+    return LatticeWomble(m, average_direction, w[1].x, w[1].y)
+end
+
+"""
+    omlwv(w::Vector{T}) where {T <: TriangulationWomble}
+
+Overall Mean Lattice-Wombling Value, as in Fortin 1994
+"""
+function omlwv(w::Vector{T}) where {T<:TriangulationWomble}
+    # Check that all wombles have the same dimensions and coordinates
+    @assert all([w1.x == w2.x for w1 in w, w2 in w])
+    @assert all([w1.y == w2.y for w1 in w, w2 in w])
+    @assert all([size(w1.m) == size(w2.m) for w1 in w, w2 in w])
+    # Prepare the matrices
+    m = fill(NaN, size(w[1].m))
+    α = fill(NaN, size(w[1].m))
+    # Fill the matrices
+    for _idx in eachindex(w[1].m)
+        ch = filter(!isnan, [womble.m[_idx] for womble in w])
+        di = filter(!isnan, [deg2rad(womble.θ[_idx]) for womble in w])
+        if !isempty(ch)
+            m[_idx] = mean(ch)
+            α[_idx] = atan(mean(sin.(di)), mean(cos.(di)))
+        end
+    end
+    average_direction = rad2deg.(α) .+ 180.0
+    return TriangulationWomble(m, average_direction, w[1].x, w[1].y)
 end
--- src/types/Womble.jl
+++ src/types/Womble.jl
@@ -48,25 +48,3 @@ mutable struct LatticeWomble{T<:Number} <: Womble
     x::Vector{T}
     y::Vector{T}
 end
-
-"""
-    MeanWomble{T <: Number} <: Womble
-
-A set of data (and co-ordinates) that are regularly arranged in space are of
-type `LatticeWomble` after having been passed through `wombling` - the fields in
-this type are
-
-- `m`, a *matrix* of rate of change at each (x,y) co-ordinate
-- `θ`, a *matrix* of direction of change at each (x,y) co-ordinate
-- `x` and `y`, the coordinates of the center of each cell
-
-Note that the type of `x` and `y` should be the same as the element type of ` m`
-and `θ`, because these values are all used when calculating the rate of change
-and that `x` and `y` correspond to latitude and longitude respectively.
-"""
-mutable struct MeanWomble{T<:Number} <: Womble
-    m::Vector{T}
-    θ::Vector{T}
-    x::Vector{T}
-    y::Vector{T}
-end
