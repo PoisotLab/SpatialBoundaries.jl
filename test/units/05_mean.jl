85b356886ef59fda9efde9ab5cf09cf1524a1dc0
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-02 09:35:49 -0400

    ✅ overall mean tested

--- test/units/05_mean.jl
+++ test/units/05_mean.jl
@@ -1,13 +1,12 @@
 module SBTestMean
 
 using SpatialBoundaries
-using Statistics
 using Test
 
 A = fill(1.0, 10, 12)
 W = [wombling(A) for i in 1:10]
 
-@test typeof(mean(W)) isa eltype(W)
+@test mean(W) isa eltype(W)
 
 
 end
