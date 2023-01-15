cea5f2ebb9dd717a7422e16f5e2759308780b91f
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-01-15 12:25:46 -0500

    AAAAAAAAAAAAA

--- docs/make.jl
+++ docs/make.jl
@@ -2,38 +2,39 @@ using Documenter
 using SpatialBoundaries
 using Literate
 
-# Generate the vignettes using Literate
+@info "Prepare to compile"
 vignettes_dir = joinpath("docs", "src", "vignettes")
 for vignette in readdir(vignettes_dir)
     if occursin(".jl", vignette)
-        Literate.markdown(joinpath(vignettes_dir, vignette), vignettes_dir; credit=false)
+        Literate.markdown(joinpath(vignettes_dir, vignette), vignettes_dir; credit = false)
     end
 end
 
-makedocs(
+@info "Prepare to build"
+makedocs(;
     sitename = "SpatialBoundaries",
     format = Documenter.HTML(),
     modules = [SpatialBoundaries],
     pages = [
         "Home" => [
             "Introduction" => "vignettes/introduction.md",
-            "SpatialBoundaries.jl" => "index.md"
-            ],
+            "SpatialBoundaries.jl" => "index.md",
+        ],
         "Vignettes" => [
             "Finding boundaries" => "vignettes/boundaries.md",
             "Triangulation wombling" => "vignettes/triangulation.md",
-            "SDM Layers" => "vignettes/simplesdmlayers.md"
-        ]
-    ]
+            "SDM Layers" => "vignettes/simplesdmlayers.md",
+        ],
+    ],
 )
 
-# Remove the geotiffs AFTER Documenter compiles the vignettes
-rm("assets/EarthEnv/"; force=true, recursive=true)
-rm("assets/WorldClim/"; force = true, recursive = true)
+@info "Prepare to remove"
+run(`find . -type f -name ".tif" -delete`)
 
-deploydocs(
-    deps=Deps.pip("pygments", "python-markdown-math"),
-    repo="github.com/PoisotLab/SpatialBoundaries.jl.git",
-    push_preview=true,
-    devbranch="main"
+@info "Prepare to deploy"
+deploydocs(;
+    deps = Deps.pip("pygments", "python-markdown-math"),
+    repo = "github.com/PoisotLab/SpatialBoundaries.jl.git",
+    push_preview = true,
+    devbranch = "main",
 )
