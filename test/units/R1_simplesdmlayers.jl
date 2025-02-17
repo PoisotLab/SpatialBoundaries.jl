15071dbe2af2520da4eaadd7a954fca1e3a655fa
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-17 11:10:11 -0500

    test: add test with known gradient

--- test/units/R1_simplesdmlayers.jl
+++ test/units/R1_simplesdmlayers.jl
@@ -18,4 +18,16 @@ W = wombling(precipitation)
 @test isa(W.rate, SDMLayer)
 @test isa(W.direction, SDMLayer)
 
+# Test with a gradient
+X = zeros(Float64, 200, 200)
+for i in 1:size(X, 1)
+    X[i, :] .= i
+end
+grad = SDMLayer(X)
+
+Z = wombling(grad)
+
+@test all(unique(values(Z.rate)) .== 1.0)
+@test all(unique(values(Z.direction)) .== 180.0)
+
 end
